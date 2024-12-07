637f6b7aa955eafe5e0f14eca0c512ba970c0a4a
Nilambar Elangbam <neslang.in@gmail.com>
2024-12-07 10:08:07 +0530

    Fixed the error in start up by Moving the ThemeProvider to a client component Properly handling the client-side functionality of theme switching

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ['latin'] })
